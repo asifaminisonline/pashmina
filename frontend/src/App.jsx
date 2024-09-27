24748ce16cdc3e05c313d205a463fa2f8cd117ee
asifaminisonline <asifaminisonline@gmail.com.com>
2024-09-27 11:29:48 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage';
-import TopNavbar from './components/Navbar';
-import TapeForm from './components/TapeForm';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import TopNavbar from "./components/Navbar";
+import TapeForm from "./components/TapeForm";
 
 function App() {
   return (
     <Router>
       <TopNavbar />
-      <TapeForm />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/add-design" element={<TapeForm />} />
       </Routes>
     </Router>
   );
